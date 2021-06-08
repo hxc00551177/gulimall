a94cc847355ce72a1df684549244b31af1cda191
hanxu <2102527169@qq.com>
2021-06-08 23:19:11 +0800

    nacos配置中心

--- gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/SmsCouponController.java
+++ gulimall-coupon/src/main/java/com/atguigu/gulimall/coupon/controller/SmsCouponController.java
@@ -6,6 +6,8 @@ import java.util.Map;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,11 +28,23 @@ import com.atguigu.common.utils.R;
  * @email hanxu6517@163.com
  * @date 2021-06-08 14:56:54
  */
+@RefreshScope
 @RestController
 @RequestMapping("coupon/smscoupon")
 public class SmsCouponController {
     @Autowired
     private SmsCouponService smsCouponService;
+
+    @Value("${coupon.user.name}")
+    private String name;
+
+    @Value("${coupon.user.age}")
+    private String age;
+
+    @RequestMapping("/test")
+    public R test(){
+        return R.ok().put("name", name).put("age",age);
+    }
 
     @RequestMapping("/listCoupon")
     public R listCoupon(){
